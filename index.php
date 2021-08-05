a337ac74517ff9e6cb7a2978dae65e554e00023f
Peter Nguyen <peternguyen707@gmail.com>
2021-08-05 09:19:14 -0700

    updates page

--- index.php
+++ index.php
@@ -58,15 +58,40 @@ function bondJSON(data){
 	//using this I can see the object in the console
 	console.log(data);
 	
+	$('#filmtitle').html(data.title);
 	//in this way we can see all of the data on the page
+	/*
 	let myData = JSON.stringify(data,null,4);
 	myData = '<pre>' + myData + '</pre>';
 	$("#output").html(myData);
+	*/
+
+
 	
 	//this works, but the text is all bunched up
 	//$("#output").text(JSON.stringify(data));
 
 }
+
+function bondTemplate(film){
+	return `
+		<div class="film">
+			<b>Film:</b> $(film.Film)<br />
+			<b>Title:</b> $(film.Title)<br />
+			<b>Year:</b> $(film.Year)<br />
+			<b>Director:</b> $(film.Director)<br />
+			<b>Producers:</b> $(film.Producers)<br />
+			<b>Writers:</b> $(film.Writers)<br />
+			<b>Composer:</b> $(film.Composer)<br />
+			<b>Bond:</b> $(film.Bond)<br />
+			<b>Budget:</b> $(film.Budget)<br />
+			<b>Box Office:</b> $(film.BoxOffice)<br />
+			<div class="pic"><img src="thumbnails/$(film.Image)" /></div>
+		</div>	
+	`;
+}
+
+
 
 </script>
 </head>
@@ -74,8 +99,9 @@ function bondJSON(data){
 	<h1>Bond Web Service</h1>
 		<a href="year" class="category">Bond Films By Year</a><br />
 		<a href="box" class="category">Bond Films By International Box Office Totals</a>
-		<h3 id="filmtitle">Title Will Go Here</h3>
+		<h3 id="filmtitle">Title Will Go Here</h3>	
 		<div id="films">
+			<!--
 			<div class="film">
 				<b>Film:</b> 1<br />
 				<b>Title:</b> Dr. No<br />
@@ -89,6 +115,7 @@ function bondJSON(data){
 				<b>Box Office:</b> $59,567,035.00<br />
 				<div class="pic"><img src="thumbnails/dr-no.jpg" /></div>
 			</div>
+			-->
 		</div>
 		<div id="output">Results go here</div>
 	</body>
